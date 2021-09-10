a4d66e0fe4dff43c5a70b52e810cc38c4317c41d
AashishA11 <76701146+AashishA11@users.noreply.github.com>
2021-09-10 21:18:09 +0530

    on click listener added on  Activitys

---
+++ app/src/main/java/com/example/effe_21ca/About_Us.java
@@ -0,0 +1,15 @@
+package com.example.effe_21ca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class About_Us extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+    }
+}
--- app/src/main/java/com/example/effe_21ca/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/effe_21ca/ui/dashboard/DashboardFragment.java
@@ -26,13 +26,13 @@ private FragmentDashboardBinding binding;
     binding = FragmentDashboardBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = binding.textDashboard;
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
