75458ade6e0670c5ad91849417bbdc20ce204bfe
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-09-16 00:58:23 +0530

    Bottom_Navigation_Activity and login page  connected

--- app/src/main/java/com/example/effe_21ca/login.java
+++ app/src/main/java/com/example/effe_21ca/login.java
@@ -2,13 +2,25 @@ package com.example.effe_21ca;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+    Button button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(login.this, Bottom_Navigation_Activity.class));
+            }
+        });
     }
 }
