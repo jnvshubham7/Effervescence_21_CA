2b4ce9fa0af1c4252304c89a77c3d543f45e09eb
Janhavi <janhavibawaskar028@gmail.com>
2021-09-15 14:20:37 +0530

    till loginPage

--- app/src/main/java/com/example/effe_21ca/MainActivity.java
+++ app/src/main/java/com/example/effe_21ca/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.effe_21ca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    SwitchCompat switchCompat;
 
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switchCompat = findViewById(R.id.switchBtn);
+
+        switchCompat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/effe_21ca/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.effe_21ca;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, login.class));
+            }
+        });
+    }
+}
