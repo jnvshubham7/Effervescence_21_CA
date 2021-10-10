4e27330b877da45c2dce44c56f65ed761e49abcb
starboi02 <iit2019091@iiita.ac.in>
2021-10-10 13:18:00 +0530

    fix bug

--- app/src/main/java/com/example/effe_21ca/Bottom_Navigation_Activity.java
+++ app/src/main/java/com/example/effe_21ca/Bottom_Navigation_Activity.java
@@ -1,16 +1,20 @@
 package com.example.effe_21ca;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -21,6 +25,10 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.effe_21ca.databinding.ActivityBottomNavigationBinding;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class Bottom_Navigation_Activity extends AppCompatActivity {
 
@@ -29,6 +37,8 @@ private ActivityBottomNavigationBinding binding;
 private DrawerLayout drawerLayout;
 private NavigationView navigationView;
 FirebaseAuth auth;
+    FirebaseStorage storage;
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +60,8 @@ FirebaseAuth auth;
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
          auth=FirebaseAuth.getInstance();
+        storage=FirebaseStorage.getInstance();
+        database=FirebaseDatabase.getInstance();
          imgnoti=findViewById(R.id.imgNotification);
          imgnoti.setOnClickListener(new View.OnClickListener() {
              @Override
@@ -89,6 +101,32 @@ FirebaseAuth auth;
         }
 
         return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("massage", "s2");
+         Toast.makeText(Bottom_Navigation_Activity.this, "image selected", Toast.LENGTH_SHORT).show();
+
+        if(data.getData()!=null){
+
+
+
+            Uri sFile=data.getData();
+
+            final StorageReference reference=storage.getReference().child("profile picture")
+                    .child(FirebaseAuth.getInstance().getUid());
+            reference.putFile(sFile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+
+                    Toast.makeText(Bottom_Navigation_Activity.this, "Image is Uploaded", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+             }
     }
 
 }
